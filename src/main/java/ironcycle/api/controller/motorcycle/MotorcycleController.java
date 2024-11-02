03772dda255ad5b47475f48c10a7823fa98f156b
FernandoMachado27 <fernando2747@yahoo.com>
2024-11-02 16:41:41 -0300

    Implementando delete

--- src/main/java/ironcycle/api/controller/motorcycle/MotorcycleController.java
+++ src/main/java/ironcycle/api/controller/motorcycle/MotorcycleController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -56,6 +58,15 @@ public class MotorcycleController {
 		motorcycle.updateMotorcycle(dataUpdateMotorcycle);
 		
 		return ResponseEntity.ok(new DataDetailsMotorcycle(motorcycle));
+	}
+	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity delete(@PathVariable Long id) {
+		var motorcycle = repository.getReferenceById(id);
+		repository.delete(motorcycle);
+		return ResponseEntity.noContent().build();
 	}
 
 }
