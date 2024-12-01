f42dc3ae43b93d538b175e2877f5abcc8eb5f2db
FernandoMachado27 <fernando2747@yahoo.com>
2024-12-01 00:56:28 -0300

    CORS Cadastro

--- src/main/java/ironcycle/api/security/SecurityConfiguration.java
+++ src/main/java/ironcycle/api/security/SecurityConfiguration.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,15 +27,23 @@ public class SecurityConfiguration implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(csrf -> csrf.disable())
+            .csrf(csrf -> csrf.disable())  
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())) // Configuração de CORS
             .authorizeHttpRequests(authorized -> authorized
                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/motorcycle").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/motorcycle").permitAll()
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers(HttpMethod.GET, "/motorcycle").permitAll()
+                .requestMatchers(HttpMethod.GET, "/users").permitAll()
+                .requestMatchers(HttpMethod.PUT, "/motorcycle").permitAll()
+                .requestMatchers(HttpMethod.PUT, "/users").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/motorcycle/**").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/users/**").permitAll()
                 .anyRequest().authenticated()
             )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
@@ -44,16 +54,6 @@ public class SecurityConfiguration implements WebMvcConfigurer {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();  // Encoder de senha BCrypt
     }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Permite todas as rotas
-                .allowedOrigins("http://127.0.0.1:5500")  // Origem permitida
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
-                .allowCredentials(true)  // Permite enviar cookies e credenciais
-                .allowedHeaders("*");  // Permite todos os cabeçalhos
-    }
-
 }
